3f5766fe2f9375520967fc12c1e3195acaea69f3
WaitingForBan <przemyslaw.michalski.85@gmail.com>
2019-10-21 19:50:39 +0200

    dodanie nowego pliku do mojego Gita

---
+++ napisy.py
@@ -0,0 +1,11 @@
+#
+# typ danych - napisy (albo string)
+#
+napis1 = "ala ma kota"
+napis2 = 'ala ma kota'
+napis3 = 'a to jest "cudzysłów" '
+napis4 = "a to jest \"cudzysłów\" "
+napis5 = "znaki specjalne: \t \n \r "
+
+dlugosc = len(napis1)
+print("dlugosc zmiennej napis 1 to ", dlugosc, "znaków")

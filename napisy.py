34f4fcee5e294791ac47e55895713185cb41a8a7
WaitingForBan <przemyslaw.michalski.85@gmail.com>
2019-10-21 20:17:34 +0200

    dodanie nowego pliku do mojego Gita

--- napisy.py
+++ napisy.py
@@ -9,3 +9,20 @@ napis5 = "znaki specjalne: \t \n \r "
 
 dlugosc = len(napis1)
 print("dlugosc zmiennej napis 1 to ", dlugosc, "znaków")
+
+# wiek = input("podaj wiek")
+# print("twoj wiek to: ",wiek.strip())
+
+s = "ruda tanczy jak szalona"
+print(s.capitalize())
+print(s.upper())
+print(s.title())
+print(s.swapcase())
+print(s.center(100))
+print(s.replace("r","d"))
+print("czy liczba: ", s.isdecimal())
+
+
+print("4 litera: ", s[3])
+print("przed ostatnia litera: ", s[-2])
+print("litery pomiedzy: ", s[0:8])

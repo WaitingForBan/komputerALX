3a79ab71de5a654e2573dcbe40960815c4e7c9d5
WaitingForBan <56680367+WaitingForBan@users.noreply.github.com>
2019-10-22 18:14:41 +0200

    Create moj_projekt.py
    nowy plik

---
+++ moj_projekt.py
@@ -0,0 +1,7 @@
+print('Cześć, jak masz na imię?') #ask for thier name
+myName = input()
+print('Miło mi Cię pozanć, ' + myName)
+print('Wow ! Wiesz, że Twoje imię ma: ' + str(len(myName)) + ' literek?')
+print('A powiesz mi ile masz lat?') #ask for thier age
+myAge = input()
+print('Przykro mi to mówić, ale w tym roku będziesz mieć już ' + str(int(myAge) + 1) + ' lat' + ' ...ale nie martw się, bo wyglądasz coraz lepiej :)')
